310680c3ddd531d5dbf130c6b43cfa3e59dec74c
Aram <Aram.10@hotmail.com>
2023-05-06 11:32:26 +0200

    commit

--- frontend/src/components/Alive.jsx
+++ frontend/src/components/Alive.jsx
@@ -1,31 +1,49 @@
-import {useState} from "react"
-import MyAPIService from "../utils/api/service/MyAPIService.js";
+import React, { useState } from 'react';
+import MyAPIService from '../utils/api/service/MyAPIService.js';
+// Importera din CSS-modul
+import styles from './Alive.module.css';
 
 const Alive = () => {
-    const [data, setData] = useState('')
+  const [data, setData] = useState('');
+  const [displayText, setDisplayText] = useState(true);
 
-    const checkApiStatus = () => {
-        MyAPIService.Alive()
-            .then((response) => {
-                setData(response.data)
-            })
-            .catch((error) => console.log(error))
+  const checkApiStatus = () => {
+    MyAPIService.Alive()
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+        setData('An error occurred while connecting to the backend.');
+      });
+  };
+
+  const displayData = () => {
+    if (data.length !== 0 && displayText) {
+      return (
+        <h3
+          className={`${styles.heading} ${
+            data.includes('error') ? styles.headingError : ''
+          }`}
+        >
+          {data}
+        </h3>
+      );
     }
+  };
 
-    const displayData = () => {
-        if (data.length !== 0) {
-            return <h3>{data}</h3>
-        }
-    }
+  const toggleDisplayText = () => {
+    setDisplayText(!displayText);
+  };
 
-    return (
-        <>
-                <button onClick={() => checkApiStatus()}>
-                    API
-                </button>
-                <h3>{displayData()}</h3>
-        </>
-    )
-}
+  return (
+    <div className={styles.container}>
+      <button className={styles.button} onClick={() => checkApiStatus()}>
+        API
+      </button>
+      {displayData()}
+    </div>
+  );
+};
 
-export default Alive
+export default Alive;
